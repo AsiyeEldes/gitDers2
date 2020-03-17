4354e0d0278934878a70dbe25db97b715fa0d740
Asiye Eldes <eldesasiye@gmail.com>
2020-03-17 22:57:01 +0200

    first my feature

--- src/gitDers2Package/GitBranch.java
+++ src/gitDers2Package/GitBranch.java
@@ -1,5 +1,7 @@
 package gitDers2Package;
 
-public class GitBranch {
-
+public class GitBranch {  
+	public static void main(String[] args) {
+		System.out.println("feature branch olusturdum");
+	}
 }
