9de7a63ee424a0c2b7a2a37b4bfec691fc2392e3
Asiye Eldes <eldesasiye@gmail.com>
2020-03-17 16:20:43 +0200

    ikinci satiri degistirdim surpriz

--- src/gitDers2Package/HelloGit.java
+++ src/gitDers2Package/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("bu 3. komit olacak" );
-		System.out.println("bu 4. komit olacak");
+		System.out.println("bu 6. komit olacak");
 		System.out.println("bu 5. komit olacak");
 		
 	}
