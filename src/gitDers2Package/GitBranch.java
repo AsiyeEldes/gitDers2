88a533352f2078c7daac47f97404bb2c6127f5e0
Asiye Eldes <eldesasiye@gmail.com>
2020-03-17 23:09:07 +0200

    feature 2. komit

--- src/gitDers2Package/GitBranch.java
+++ src/gitDers2Package/GitBranch.java
@@ -3,5 +3,6 @@ package gitDers2Package;
 public class GitBranch {  
 	public static void main(String[] args) {
 		System.out.println("feature branch olusturdum");
+		System.out.println("hikayeler :) feature branch inda ikin. komit");
 	}
 }
